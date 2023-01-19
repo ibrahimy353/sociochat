cbe7b63b909d94ab6944fd1e2c81112905963407
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-19 17:25:36 +0300

    added changes

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -27,6 +27,21 @@ import FlexBetween from "../../components/FlexBetween";
 
 
 const NavBar = () => {
+    const [isMobilMenuToggled, setMobileMenuToggled] = useState(false);//allows us to determine the value when in small screen
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state);
+    const isNonMobileScreens = useMediaQuery("(min-width = 1000px)");//this is a hook build in mui to determine whether the current screen is below the min width of higher
+    
+    const theme =useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const backgroung = theme.palette.background.default;
+    const primaryLight = theme.palatte.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${ user.firstName} ${user.lastName}`;
+    
     return <div>navbar</div>;
 };
 
