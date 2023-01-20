36667eaadacc1ffead98fe1b8d3ffcdacecbf7ab
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-20 17:06:59 +0300

    added auth on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector ((state) => state.mode);
   const theme = useMemo(() => createTheme( themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token))
 
   return ( 
   <div className="App">
@@ -21,7 +22,7 @@ function App() {
     <Routes>
       <Route path="/" element= {<LoginPage/>} />
       {/* sets the path that user will log into*/}  
-      <Route path="/home" element= {<HomePage/>} />
+      <Route path="/home" element= {isAuth  ? <HomePage/> : <Navigate to ="/" />} />
       <Route path="/profile/:userId" 
       element= {<ProfilePage/>} />
     </Routes>
