3bcbb6b9fbcb8e3b4436b9a7f53f5e9b31c3b9f7
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-21 19:37:26 +0300

    added postswidget

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -5,8 +5,8 @@ import PostWidget from "./PostWidget";
 
 const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts);
-  const token = useSelector((state) => state.token);
+  const posts = useSelector((state) => state.posts);//grabs stored list of post
+  const token = useSelector((state) => state.token);//grabs the list of token
 
   const getPosts = async () => {
     const response = await fetch("http://localhost:3001/posts", {
