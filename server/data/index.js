b2a1ab8403622d8dd5a1036ceacf1f57a3302ca9
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-22 22:22:04 +0300

    data info addition

--- server/data/index.js
+++ server/data/index.js
@@ -14,8 +14,8 @@ const userIds = [
 export const users = [
   {
     _id: userIds[0],
-    firstName: "test",
-    lastName: "me",
+    firstName: "ibrahim",
+    lastName: "yusuf",
     email: "aaaaaaa@gmail.com",
     password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
     picturePath: "p11.jpeg",
