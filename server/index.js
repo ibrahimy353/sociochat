4937de729a42bc4c6fdf98964187e5d38ac86e65
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-27 17:04:55 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
