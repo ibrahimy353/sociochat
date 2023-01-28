c5a6d0670430829b87f89ddab6387533c28ab744
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-28 19:22:41 +0300

    modify

--- server/data/index.js
+++ server/data/index.js
@@ -111,7 +111,7 @@ export const users = [
   {
     _id: userIds[6],
     firstName: "Carly",
-    lastName: "rae",
+    lastName: "Japson",
     email: "carlyvowel@gmail.com",
     password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
     picturePath: "p8.jpeg",
